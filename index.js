e05a2233bc358aa5c6bf17526d39389a69e5f5b7
AlexKT1022 <alexkt1022@gmail.com>
2025-07-23 12:03:04 -0400

    workshop complete

---
+++ index.js
@@ -0,0 +1,25 @@
+// part 1
+let code1;
+
+// part 2
+code1 = 7 + 3;
+
+// part 3
+const code2 = (100 - 20) / 2;
+
+// part 4
+const code3 = 39 % 47;
+
+// part 5
+const message = 'The vault has been secured. The combination is: ';
+
+// part 6
+const codeA = code1 + '-' + code2 + '-' + code3;
+
+// part 7
+const codeB = `${code1}-${code2}-${code3}`;
+
+// part 8
+console.log(message);
+console.log(codeA);
+console.log(codeB);
